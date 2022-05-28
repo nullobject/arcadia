b14a539c3c63644b4ba9c48cdd6fc935935c73e5
Joshua Bassett <hello@joshbassett.info>
2022-05-28 12:55:04 +1000

    Update BurstBufferTest

--- src/test/scala/axon/mem/buffer/BurstBufferTest.scala
+++ src/test/scala/axon/mem/buffer/BurstBufferTest.scala
@@ -138,6 +138,126 @@ class BurstBufferTest extends AnyFlatSpec with ChiselScalatestTester with Matche
     }
   }
 
+  it should "buffer data (16:32)" in {
+    test(mkBuffer(bufferConfig.copy(inDataWidth = 16, outDataWidth = 32))) { dut =>
+      dut.io.in.wr.poke(true)
+
+      // write 0
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x00)
+      dut.io.in.din.poke(0x3412)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // write 1
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x02)
+      dut.io.in.din.poke(0x7856)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // wait for burst
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.waitReq.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.burstLength.expect(1)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect("h_12345678".U)
+      dut.io.out.mask.expect(0xf)
+      dut.clock.step()
+
+      // burst 0
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect("h_12345678".U)
+      dut.clock.step()
+
+      // write 2
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x04)
+      dut.io.in.din.poke(0xab90)
+      dut.io.out.burstDone.poke(false)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // write 3
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x06)
+      dut.io.in.din.poke(0xefcd)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // burst 1
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x04)
+      dut.io.out.din.expect("h_90abcdef".U)
+    }
+  }
+
+  it should "buffer data (16:32) swap endianness" in {
+    test(mkBuffer(bufferConfig.copy(inDataWidth = 16, outDataWidth = 32, swapEndianness = true))) { dut =>
+      dut.io.in.wr.poke(true)
+
+      // write 0
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x00)
+      dut.io.in.din.poke(0x3412)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // write 1
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x02)
+      dut.io.in.din.poke(0x7856)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // wait for burst
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.waitReq.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.burstLength.expect(1)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect("h_78563412".U)
+      dut.io.out.mask.expect(0xf)
+      dut.clock.step()
+
+      // burst 0
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect("h_78563412".U)
+      dut.clock.step()
+
+      // write 2
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x04)
+      dut.io.in.din.poke(0xab90)
+      dut.io.out.burstDone.poke(false)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // write 3
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x06)
+      dut.io.in.din.poke(0xefcd)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // burst 1
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x04)
+      dut.io.out.din.expect("h_efcdab90".U)
+    }
+  }
+
   it should "buffer data (32:8)" in {
     test(mkBuffer(bufferConfig.copy(inDataWidth = 32, outDataWidth = 8, burstLength = 4))) { dut =>
       dut.io.in.wr.poke(true)
@@ -223,6 +343,130 @@ class BurstBufferTest extends AnyFlatSpec with ChiselScalatestTester with Matche
       dut.io.out.wr.expect(true)
       dut.io.out.addr.expect(0x04)
       dut.io.out.din.expect(0xef)
+    }
+  }
+
+  it should "buffer data (32:16)" in {
+    test(mkBuffer(bufferConfig.copy(inDataWidth = 32, outDataWidth = 16, burstLength = 2))) { dut =>
+      dut.io.in.wr.poke(true)
+
+      // write 0
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x00)
+      dut.io.in.din.poke("h_78563412".U)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // wait for burst
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.waitReq.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.burstLength.expect(2)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect(0x3412)
+      dut.io.out.mask.expect(0x3)
+
+      // burst 0
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.waitReq.poke(false)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect(0x3412)
+      dut.clock.step()
+
+      // burst 1
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect(0x7856)
+      dut.clock.step()
+
+      // write 1
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x04)
+      dut.io.in.din.poke("h_efcdab90".U)
+      dut.io.out.burstDone.poke(false)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // burst 2
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.burstLength.expect(2)
+      dut.io.out.addr.expect(0x04)
+      dut.io.out.din.expect(0xab90)
+      dut.io.out.mask.expect(0x3)
+      dut.clock.step()
+
+      // burst 3
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x04)
+      dut.io.out.din.expect(0xefcd)
+    }
+  }
+
+  it should "buffer data (32:16) swap endianness" in {
+    test(mkBuffer(bufferConfig.copy(inDataWidth = 32, outDataWidth = 16, burstLength = 2, swapEndianness = true))) { dut =>
+      dut.io.in.wr.poke(true)
+
+      // write 0
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x00)
+      dut.io.in.din.poke("h_78563412".U)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // wait for burst
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.waitReq.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.burstLength.expect(2)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect(0x3412)
+      dut.io.out.mask.expect(0x3)
+
+      // burst 0
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.waitReq.poke(false)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect(0x3412)
+      dut.clock.step()
+
+      // burst 1
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x00)
+      dut.io.out.din.expect(0x7856)
+      dut.clock.step()
+
+      // write 1
+      dut.io.in.waitReq.expect(false)
+      dut.io.in.addr.poke(0x04)
+      dut.io.in.din.poke("h_efcdab90".U)
+      dut.io.out.burstDone.poke(false)
+      dut.io.out.wr.expect(false)
+      dut.clock.step()
+
+      // burst 2
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.burstLength.expect(2)
+      dut.io.out.addr.expect(0x04)
+      dut.io.out.din.expect(0xab90)
+      dut.io.out.mask.expect(0x3)
+      dut.clock.step()
+
+      // burst 3
+      dut.io.in.waitReq.expect(true)
+      dut.io.out.burstDone.poke(true)
+      dut.io.out.wr.expect(true)
+      dut.io.out.addr.expect(0x04)
+      dut.io.out.din.expect(0xefcd)
     }
   }
 }
